6a5aa41f6b646633a16dccbe19b3ceae6f7c4860
= <=>
2021-12-05 17:36:43 -0300

    category repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -2,23 +2,31 @@ import { Router } from "express";
 import { v4 as uuidv4 } from 'uuid';
 import Category from "../model/Category";
 
-const categoriesRoutes = Router()
+import CategoryRepository from "../repositories/CategoryRepository";
 
-const categories: Category[] = []
+const categoriesRoutes = Router()
+const categoryRepository = new CategoryRepository();
+
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category = new Category();
-    Object.assign(category, {
-        name,
-        description,
-        created_at: new Date(),
-    })
+    const categoryAlreadyExists = categoryRepository.findByName(name)
 
-    categories.push(category)
+    if (categoryAlreadyExists) {
+        return response.status(400).json({ error: "Category alredy exists!" })
+    }
+
+    categoryRepository.create({ name, description })
 
     return response.status(201).send();
+})
+
+categoriesRoutes.get("/", (request, response) => {
+
+    const categories = categoryRepository.list()
+
+    return response.json(categories);
 })
 
 export { categoriesRoutes }
